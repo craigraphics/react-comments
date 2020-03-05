f1150646c873b2f97e23d5fcc56bcf8cef067614
craigraphics <craigraphics@gmail.com>
2020-03-05 15:58:04 -0600

    action creators and reducers added

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,21 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchPosts } from "../actions";
+import { fetchPostsAndUsers } from "../actions";
+
+import UserHeader from "./UserHeader";
 
 class PostList extends React.Component {
   componentDidMount() {
-    this.props.fetchPosts();
+    this.props.fetchPostsAndUsers();
   }
   renderList() {
     return this.props.posts.map(post => {
       return (
-        <div className="item" key="post.id">
+        <div className="item" key={post.id}>
           <i className="large middle aligned icon user" />
           <div className="content">
             <div className="description">
               <h2>{post.title}</h2>
               <p>{post.body}</p>
             </div>
+            <UserHeader userId={post.userId}></UserHeader>
           </div>
         </div>
       );
@@ -32,5 +35,5 @@ const mapStateToProps = state => {
 
 export default connect(
   mapStateToProps,
-  { fetchPosts }
+  { fetchPostsAndUsers }
 )(PostList);
